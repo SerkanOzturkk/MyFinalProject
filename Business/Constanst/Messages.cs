f3dfb1152c826299b268d370d8a957a0aadb0c9a
Serkan Öztürk <serkanoztrk237@gmail.com>
2024-07-28 22:41:43 +0300

    Results added

---
+++ Business/Constanst/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace Business.Constanst
+{
+    public static class Messages
+    {
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+    }
+}
